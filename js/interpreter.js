b6fce5ab4280d1849ad333b04a25aff3e0a29646
delucks <jamie.delucks@gmail.com>
2016-05-01 01:08:02 +0000

    Changed pretty much everything. Removed external dependencies (except jquery terminal), cleaned up the source a bunch, made the styles simpler

--- js/interpreter.js
+++ js/interpreter.js
@@ -1,6 +1,4 @@
 /* interpreter.js
- * part of delucks' site additions, 05/24/2015
- * was changed a bunch of times since then
  */
 
 // Websites that we'll use throughout the document
@@ -9,15 +7,41 @@ var github = 'https://github.com/delucks';
 var linkedin = 'https://www.linkedin.com/in/jamieluck';
 var resume = 'http://www.jamesluck.com/JamesLuckResume.pdf';
 var lastfm = 'https://www.last.fm/user/delucks';
-var lug = 'http://lug.udel.edu';
 var links = [
-	["home", home],
+	[".", home],
 	["github", github],
 	["linkedin", linkedin],
 	["resume", resume],
-	["lastfm", lastfm],
-	["lug", lug]
+	["lastfm", lastfm]
 ];
+
+var help_output = "<table><tr>" +
+"<td><span style='color: #526f33'>ping</span></td><td>pong</td>" +
+"</tr><tr>" +
+"<td><span style='color: #526f33'>contact</span></td><td>my contact information</td>" +
+"</tr><tr>" +
+"<td><span style='color: #526f33'>ls</span></td><td>links to other places (are clickable)</td>" +
+"</tr><tr>" +
+"<td><span style='color: #526f33'>cd</span></td><td>open one of my links by name</td>" +
+"</tr><tr>" +
+"<td><span style='color: #526f33'>help</span></td><td>display this help text</td>" +
+"</tr><tr>" +
+"<td><span style='color: #526f33'>clear</span></td><td>clear the terminal</td>" +
+"</tr><tr>" +
+"<td><span style='color: #526f33'>exit</span></td><td>exit to my graphical website</td>" +
+"</tr></table>"
+
+var ping_output = "PING www.jamesluck.com (127.0.0.1) 56(84) bytes of data.\n" +
+"64 bytes from www.jamesluck.com: icmp_seq=1 ttl=59 time=0.001 ms\n\n" +
+"--- www.jamesluck.com ping statistics ---\n" +
+"1 packets transmitted, 1 received, 0% packet loss, time 0ms\n" +
+"rtt min/avg/max/mdev = 0.001/0.001/0.001/0.000 ms"
+
+var contact_output = "<pre>echo 'nv(qznvhofxp)xln' | tr \"$(echo {z..a} | tr -d ' ')()\" 'a-z@.' # email</pre>" +
+"If you want to get to me faster, find me on IRC:<br>" +
+"username: <span style='color: #526f33'>delucks</span><br>" +
+"network: <span style='color: #526f33'>&#160irc.lug.udel.edu</span><br>" +
+"channel: <span style='color: #526f33'>&#160#lug</span></span><br>"
 
 // Set up the terminal
 $(document).ready(function() {
@@ -26,19 +50,18 @@ $(document).ready(function() {
 			this.echo(arg1)
 		},
     ping: function() {
-      this.echo("PING www.jamesluck.com (127.0.0.1) 56(84) bytes of data.");
-      this.echo("64 bytes from www.jamesluck.com: icmp_seq=1 ttl=59 time=0.001 ms");
-      this.echo("\n")
-      this.echo("--- www.jamesluck.com ping statistics ---");
-      this.echo("1 packets transmitted, 1 received, 0% packet loss, time 0ms");
-      this.echo("rtt min/avg/max/mdev = 0.001/0.001/0.001/0.000 ms");
-      this.echo("\n")
-      this.echo("hi k");
+      this.echo(ping_output)
     },
 		cd: function(arg1) {
 			switch (arg1)
 			{
-				case "home":
+				case "$HOME":
+					window.open(home,'_blank');
+					break;
+				case ".":
+					window.open(home,'_blank');
+					break;
+				case "~":
 					window.open(home,'_blank');
 					break;
 				case "github":
@@ -53,9 +76,9 @@ $(document).ready(function() {
 				case "lastfm":
 					window.open(lastfm,'_blank');
 					break;
-				case "lug":
-					window.open(lug,'_blank');
-					break;
+        case "..":
+          this.echo("You're at the top level, silly");
+          break;
 				default:
 					this.echo("I can't find " + arg1);
 			}
@@ -63,25 +86,13 @@ $(document).ready(function() {
 		cat: function(arg1) {
 			this.exec("cd " + arg1);
 		},
+    exit: function() {
+      $('#term').toggle();
+    },
 		help: function() {
-			this.echo("\
-					<table><tr>\
-					<td><span style='color: #526f33'>about</span></td><td>   about me</td>\
-					</tr><tr>\
-					<td><span style='color: #526f33'>ping</span></td><td>   pong</td>\
-					</tr><tr>\
-					<td><span style='color: #526f33'>contact</span></td><td>   my contact information</td>\
-					</tr><tr>\
-					<td><span style='color: #526f33'>ls</span></td><td>   links to other places (are clickable)</td>\
-					</tr><tr>\
-					<td><span style='color: #526f33'>cd</span></td><td>   open one of my links by name</td>\
-					</tr><tr>\
-					<td><span style='color: #526f33'>help</span></td><td>   display this help text</td>\
-					</tr><tr>\
-					<td><span style='color: #526f33'>clear</span></td><td>   clear the terminal</td>\
-					</tr></table>",{"raw":"true"});
+      this.echo(help_output, {raw: true});
 		},
-		ls: function() {
+		ls: function(arg) {
 			var linktext = "<p style='color: #526f33'>Links are directories, cd into them to view them. You can also click on the directory names.</p><table>";
 			for (var i = 0; i<links.length;i++) {
 				linktext = linktext + "<tr><td>drwx-r-xr-x</td><td>2</td><td>delucks</td><td>users</td><td>4096</td><td>May 24 02:21</td><td><a href='"+links[i][1]+"' target='_blank'>"+links[i][0]+"</a></td></tr>";
@@ -90,34 +101,15 @@ $(document).ready(function() {
 			this.echo(linktext,{"raw":"true"});
 		},
 		contact: function() {
-			this.echo("\
-				You can email me at<br \>\
-				<span style='color: #526f33'>me</span> AT \
-				<span style='color: #526f33'>jamesluck </span> DOT <span style='color: #526f33'>com</span><br \><br \>\
-				If you want to get to me faster, find me on IRC:<br \>\
-				username: <span style='color: #526f33'>delucks</span><br \>\
-				network: <span style='color: #526f33'>&#160irc.lug.udel.edu</span><br \>\
-				channel: <span style='color: #526f33'>&#160#lug</span></span>\
-				<br>\
-				", {raw: true});
+      this.echo(contact_output, {raw: true});
 		},
-		about: function() {
-			this.echo("My name is James Luck, and this is my website. <br>There's a lot more information on here than it seems :)", {raw: true});
-		}
 	}, {
-		prompt: 'visitor@delucks ~ ; ', 
-		name:'urxvt', 
-		height:'400px',
-		width:'600px',
+		prompt: 'visitor@localhost ~ . ', 
+    height: '99%', /* cannot do 100% because it obscures the prompt for whatever reason */
+    exit: false,
 		greetings:greeter
 		});
 	function greeter() {
-		return 'Thanks for stopping by! Type "help" for a list of commands.\nClick my face to toggle the terminal.'
+		return 'Hi! If you want to see a more traditional website, type \'exit\'\nThe command "help" is a good starting place.'
 	}
-
-	$('.floating').draggable().resizable();
-  $('#headshot').click(function() {
-    $('#term').slideToggle("fast");
-  });
-
 });
