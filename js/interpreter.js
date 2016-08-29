dfc683f8c28e2532a0c2b82c1a78e4c9a7c1e9e1
delucks <jamie.delucks@gmail.com>
2016-08-28 20:22:36 -0400

    Updated jquery-terminal to v0.10.12, fixed stylistic bugs that resulted

--- js/interpreter.js
+++ js/interpreter.js
@@ -16,28 +16,28 @@ var links = [
 ];
 
 var help_output = "<table><tr>" +
-"<td><span style='color: #526f33'>ping</span></td><td>pong</td>" +
+"<td><span class='term-green'>ping</span></td><td>pong</td>" +
 "</tr><tr>" +
-"<td><span style='color: #526f33'>contact</span></td><td>my contact information</td>" +
+"<td><span class='term-green'>contact</span></td><td>my contact information</td>" +
 "</tr><tr>" +
-"<td><span style='color: #526f33'>ls</span></td><td>links to other places (are clickable)</td>" +
+"<td><span class='term-green'>ls</span></td><td>links to other places (are clickable)</td>" +
 "</tr><tr>" +
-"<td><span style='color: #526f33'>cd</span></td><td>open one of my links by name</td>" +
+"<td><span class='term-green'>cd</span></td><td>open one of my links by name</td>" +
 "</tr><tr>" +
-"<td><span style='color: #526f33'>help</span></td><td>display this help text</td>" +
+"<td><span class='term-green'>help</span></td><td>display this help text</td>" +
 "</tr><tr>" +
-"<td><span style='color: #526f33'>clear</span></td><td>clear the terminal</td>" +
+"<td><span class='term-green'>clear</span></td><td>clear the terminal</td>" +
 "</tr><tr>" +
-"<td><span style='color: #526f33'>exit</span></td><td>exit to my graphical website</td>" +
-"</tr></table>"
+"<td><span class='term-green'>exit</span></td><td>exit to my graphical website</td>" +
+"</tr></table>";
 
 var ping_output = "PING jamesluck.com (127.0.0.1) 56(84) bytes of data.\n" +
 "64 bytes from jamesluck.com: icmp_seq=1 ttl=59 time=0.001 ms\n\n" +
 "--- jamesluck.com ping statistics ---\n" +
 "1 packets transmitted, 1 received, 0% packet loss, time 0ms\n" +
-"rtt min/avg/max/mdev = 0.001/0.001/0.001/0.000 ms"
+"rtt min/avg/max/mdev = 0.001/0.001/0.001/0.000 ms";
 
-var contact_output = "<pre>echo 'nv(qznvhofxp)xln' | tr \"$(echo {z..a} | tr -d ' ')()\" 'a-z@.' # email</pre>"
+var contact_output = "<pre>echo 'nv(qznvhofxp)xln' | tr \"$(echo {z..a} | tr -d ' ')()\" 'a-z@.' # email</pre>";
 
 // Set up the terminal
 $(document).ready(function() {
@@ -88,21 +88,22 @@ $(document).ready(function() {
 		help: function() {
       this.echo(help_output, {raw: true});
 		},
-		ls: function(arg) {
-			var linktext = "<p style='color: #526f33'>Links are directories, cd into them to view them. You can also click on the directory names.</p><table>";
+		ls: function() {
+			var linktext = "<p class='term-green'>Links are directories, cd into them to view them. You can also click on the directory names.</p><table>";
 			for (var i = 0; i<links.length;i++) {
-				linktext = linktext + "<tr><td>drwx-r-xr-x</td><td>2</td><td>delucks</td><td>users</td><td>4096</td><td>May 24 02:21</td><td><a href='"+links[i][1]+"' target='_blank'>"+links[i][0]+"</a></td></tr>";
+				linktext = linktext + "<tr><td>drwx-r-xr-x</td><td>2</td><td>delucks</td><td>users</td><td>4096</td><td>Aug 28 19:51</td><td><a class='term-green' href='"+links[i][1]+"' target='_blank'>"+links[i][0]+"</a></td></tr>";
 			}
 			linktext = linktext + "</table>";
-			this.echo(linktext,{"raw":"true"});
+			this.echo(linktext, {raw: true, exec: false});
 		},
 		contact: function() {
       this.echo(contact_output, {raw: true});
 		},
 	}, {
-		prompt: 'visitor@localhost ~ . ', 
+		prompt: 'visitor@jamesluck.com:~$ ', 
     height: '99%', /* cannot do 100% because it obscures the prompt for whatever reason */
     exit: false,
+    convertLinks: false,
 		greetings:greeter
 		});
 	function greeter() {
